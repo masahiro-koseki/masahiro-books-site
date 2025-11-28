f3b47cf26073fa8ab13d6e3cede2b263ca367daf
Masahiro Koseki <manbo@MacBook-Air.local>
2025-11-29 02:05:53 +0900

    Change hero image bg

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -59,7 +59,7 @@ export default function HeroSection({ texts }: { texts: HeroTexts }) {
 		
 		{/* ▼ スライド画像 */}
 		<div className="relative w-full mt-10 flex justify-center">
-		<div className="relative w-full max-w-2xl aspect-video overflow-hidden rounded-xl bg-transparent">
+		<div className="relative w-full max-w-3xl aspect-video overflow-hidden rounded-xl bg-transparent">
 		{sliderImages.map((src, i) => (
 					<Image
 					key={i}
