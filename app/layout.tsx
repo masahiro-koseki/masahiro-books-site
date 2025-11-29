d615310d4f6a66480cf2ce7a9bf24d503642c5f2
Masahiro Koseki <manbo@MacBook-Air.local>
2025-11-29 18:37:17 +0900

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,11 @@ export const metadata: Metadata = {
 	
 	icons: {
 		icon: [
+		{ url: "/favicon-16.png", sizes: "16x16", type: "image/png" },
 		{ url: "/favicon-32.png", sizes: "32x32", type: "image/png" },
 		{ url: "/favicon-48.png", sizes: "48x48", type: "image/png" },
 		{ url: "/favicon-64.png", sizes: "64x64", type: "image/png" },
+		{ url: "/favicon.ico" },
 		],
 		apple: "/apple-touch-icon.png",
 	}
